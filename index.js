e97fd9c1ff31964234551ef314f2e866464e67b2
Puneet Saini <puneet29saini@gmail.com>
2019-06-01 13:50:45 +0530

    Basic menu added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const electron = require('electron');
 
 // Getting the properties from dependencies using destructuring
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 // Defining main window
 let mainWindow;
@@ -14,5 +14,21 @@ app.on('ready', () => {
             nodeIntegration: true
         }
     });
-    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.loadURL(`file://${__dirname}/main.html`); // load main HTML window
+
+    // Adding menu to the app
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// Template used to make menu
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo'
+            }
+        ]
+    }
+]
